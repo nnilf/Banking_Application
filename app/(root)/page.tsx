dd388d7e0607bc6bd3fe7f6adf05c525d34d455e
Flinn Mort <flinnmort13@outlook.com>
2025-05-21 21:46:28 +0100

    implement visual bank chart and mobile nav menu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,31 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                Welcome, Flinn
-            </header>
-        </div>
-    </section>
-  )
+    const loggedIn = { firstName: 'Flinn'}
+
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manager your account
+                        and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1230.45}
+                    />
+                </header>
+            </div>
+        </section>
+    )
 }
 
 export default Home
